8ff29b623837c705d5e8a6266865140e2827a1d7
Jesse Parent <jesseparent@gmail.com>
2020-08-27 00:12:04 -0600

    icons and bg colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -200,16 +200,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    //console.log("activate", this);
+    $(this).addClass("dropover");
+    $('.bottom-trash').addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
-    //console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $('.bottom-trash').removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    //console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
-    //console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function (event) {
     // array to store the task data in
@@ -251,10 +253,10 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function (event, ui) {
-    console.log("over");
+    $('.bottom-trash').addClass("bottom-trash-active");
   },
   out: function (event, ui) {
-    console.log("out");
+    $('.bottom-trash').removeClass("bottom-trash-active");
   }
 });
 
@@ -280,6 +282,12 @@ var auditTask = function (taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 };
+
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, 1800000);
 
 // load tasks for the first time
 loadTasks();
